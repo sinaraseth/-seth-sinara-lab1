367dbf921829674af474aa5cee18a75ed5b0d22b
sinraseth <sinraseth588@gmail.com>
2025-10-31 16:01:53 +0700

    Add deployment stage to Render

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello, CI/CD!");
+  res.send("Hello, CI/CD!. Done with Deployment to Render!");
 });
 module.exports = app;
