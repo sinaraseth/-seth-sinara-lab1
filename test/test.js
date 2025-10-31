98cc58d133a257bed83f8a4783687d07e5adcc7c
sinraseth <sinraseth588@gmail.com>
2025-10-31 14:58:38 +0700

    Update greeting message

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("should return Hello, GitHub Actions!", async () => {
     const res = await request(app).get("/");
     expect(res.status).to.equal(200);
-    expect(res.text).to.equal("Hello, GitHub Actions!");
+    expect(res.text).to.equal("Hello, CI/CD!");
   });
 });
